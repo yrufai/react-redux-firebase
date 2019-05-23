4a1608daa4ce6362cf07e33a42501b130804e9cf
yakubu rufai mahatma <yrufai23@gmail.com>
2019-05-23 21:05:19 +0000

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+const Navbar = () => (
+  <nav className="nav-wrapper grey darken-3">
+    <div className="container">
+      <Link to="/" className="brand-logo">
+        Ghandhi land
+      </Link>
+      <SignedInLinks />
+    </div>
+  </nav>
+);
+
+export default Navbar;
