47b19ceba5d6658d6c2adebc166ed7b7de05570f
yakubu rufai mahatma <yrufai23@gmail.com>
2019-05-23 21:12:47 +0000

    navbar brand name

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
+
 const Navbar = () => (
   <nav className="nav-wrapper grey darken-3">
     <div className="container">
       <Link to="/" className="brand-logo">
-        Ghandhi land
+        Ghaandhi land
       </Link>
       <SignedInLinks />
     </div>
