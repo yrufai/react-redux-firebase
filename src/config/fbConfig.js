10b64bbdfdf5c6eb5b2018a061d0c9f495dd71c4
yakubu rufai mahatma <yrufai23@gmail.com>
2019-05-26 22:59:11 +0000

    firebase timestampsinsnapshots settings removed

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -13,6 +13,4 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().settings({ timestampsInSnapshots: true });
-
 export default firebase;
