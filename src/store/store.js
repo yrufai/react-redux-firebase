488d27f1ab03a807dbbad815467cae6c169ea7c4
yakubu rufai mahatma <yrufai23@gmail.com>
2019-05-29 23:56:36 +0000

    store modified to v2 of react-redux-firebase

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,33 @@
 import rrfConfig from "../config/rrfConfig";
-import { reduxFirestore } from "redux-firestore";
-import { reactReduxFirebase } from "react-redux-firebase";
-import { createStore, compose } from "redux";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "./reducers/rootReducer";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import firebaseConfig from "../config/fbConfig";
+import thunk from "redux-thunk";
 
 firebase.initializeApp(firebaseConfig);
+firebase.firestore();
 
-const createStoreWithFirebase = compose(
+/*const createStoreWithFirebase = compose(
+  thunk.withExtraArgument(getFirebase, getFirestore),
   reduxFirestore(firebase),
-  reactReduxFirebase(firebase, rrfConfig)
-)(createStore);
+  reactReduxFirebase(firebaseConfig, rrfConfig)
+)(createStore);*/
 
 const initialState = {};
 
-const store = createStoreWithFirebase(rootReducer, initialState);
+const store = createStore(
+  rootReducer(),
+  initialState,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase)
+  )
+);
 
 export default store;
